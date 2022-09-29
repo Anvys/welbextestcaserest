4071e9dd39c6f83f0c8346d973b32c2f67c534d4
Anvys <houk05@yandex.ru>
2022-09-29 13:29:29 +0300

    dist add

--- dist/db/dbconnect.js
+++ dist/db/dbconnect.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDB = void 0;
 const pg_1 = require("pg");
 const getDB = (pass, dbPort, dbName, user, host) => new pg_1.Pool({
-    user: `postgres`,
+    user: user,
     password: pass,
-    host: `localhost`,
+    host: host,
     port: dbPort,
     database: dbName
 });
